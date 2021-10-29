cdf31dfc4731e86687f96ca9135244d090009737
Carlos <carlos@falconmasters.com>
2021-10-28 19:04:19 -0500

    Agregando readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Menú de navegación pegajoso y animado
+### [Tutorial: https://youtu.be/06f80GVQ1B8](https://youtu.be/06f80GVQ1B8)
+
+![Menú de navegación pegajoso y animado](https://raw.githubusercontent.com/falconmasters/slider-nav/master/img/thumb.png)
+
+Por: [FalconMasters](http://www.falconmasters.com)
---
+++ app.js
@@ -0,0 +1,45 @@
+const menu = document.getElementById('menu');
+const indicador = document.getElementById('indicador');
+const secciones = document.querySelectorAll('.seccion');
+
+let tamañoIndicador = menu.querySelector('a').offsetWidth;
+indicador.style.width = tamañoIndicador + 'px';
+
+let indexSeccionActiva;
+
+// Observer
+const observer = new IntersectionObserver((entradas, observer) => {
+	entradas.forEach(entrada => {
+		if(entrada.isIntersecting){
+			// Obtenemos cual es la seccion que esta entrando en pantalla.
+			// console.log(`La entrada ${entrada.target.id} esta intersectando`);
+
+			// Creamos un arreglo con las secciones y luego obtenemos el index del la seccion que esta en pantalla.
+			indexSeccionActiva = [...secciones].indexOf(entrada.target);
+			indicador.style.transform = `translateX(${tamañoIndicador * indexSeccionActiva}px)`;
+		}
+	});
+}, {
+	rootMargin: '-80px 0px 0px 0px',
+	threshold: 0.2
+});
+
+// Agregamos un observador para el hero.
+observer.observe(document.getElementById('hero'));
+
+// Asignamos un observador a cada una de las secciones
+secciones.forEach(seccion => observer.observe(seccion));
+
+// Evento para cuando la pantalla cambie de tamaño.
+const onResize = () => {
+	// Calculamos el nuevo tamaño que deberia tener el indicador.
+	tamañoIndicador = menu.querySelector('a').offsetWidth;
+
+	// Cambiamos el tamaño del indicador.
+	indicador.style.width = `${tamañoIndicador}px`;
+
+	// Volvemos a posicionar el indicador.
+	indicador.style.transform = `translateX(${tamañoIndicador * indexSeccionActiva}px)`;
+}
+
+window.addEventListener('resize', onResize);
